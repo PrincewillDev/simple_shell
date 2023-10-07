5c8081264d0b3fe437c228d1dc04f44955d7eda7
PrincewillDev <princewillfidelis1@gmail.com>
2023-10-07 17:14:25 +0100

    Fixed path handling bug

--- executing.c
+++ executing.c
@@ -6,49 +6,64 @@ void executing(char **arrof_str, char **envp)
 
     if (child_process == 0)
     {
-        char *command = arrof_str[0]; /* The first argument is the command */
-        char *fullPath = _getenv("PATH"); /* Get the PATH environment variable */
-        char *path;
-        int found = 0;
-
-        /* Tokenize the PATH into individual directories */
-        char *token = strtok(fullPath, ":");
-        while (token != NULL)
+        if (arrof_str[0][0] == '/')
         {
-            /* Construct the full path to the executable */
-            path = (char *)malloc(strlen(token) + strlen(command) + 2);
-            if (path == NULL)
+            if (execve(arrof_str[0], arrof_str, envp) == -1)
+                {
+                    perror("execve failed");
+                    exit(EXIT_FAILURE);
+                }
+
+        }
+        else 
+        {
+
+        
+            char *command = arrof_str[0]; /* The first argument is the command */
+            char *fullPath = _getenv("PATH"); /* Get the PATH environment variable */
+            char *path;
+            int found = 0;
+
+            /* Tokenize the PATH into individual directories */
+            char *token = strtok(fullPath, ":");
+            while (token != NULL)
             {
-                perror("malloc failed");
+                /* Construct the full path to the executable */
+                path = (char *)malloc(strlen(token) + strlen(command) + 2);
+                if (path == NULL)
+                {
+                    perror("malloc failed");
+                    exit(EXIT_FAILURE);
+                }
+                strcpy(path, token);
+                strcat(path, "/");
+                strcat(path, command);
+
+                /* Check if the file exists and is executable */
+                if (access(path, X_OK) == 0)
+                {
+                    found = 1;
+                    break;
+                }
+
+                free(path);
+                token = strtok(NULL, ":");
+            }
+
+            if (found)
+            {
+                if (execve(path, arrof_str, envp) == -1)
+                {
+                    perror("execve failed");
+                    exit(EXIT_FAILURE);
+                }
+            }
+            else
+            {
+                /*%s: command not found\n", command*/
+                perror("./hsh");
                 exit(EXIT_FAILURE);
             }
-            strcpy(path, token);
-            strcat(path, "/");
-            strcat(path, command);
-
-            /* Check if the file exists and is executable */
-            if (access(path, X_OK) == 0)
-            {
-                found = 1;
-                break;
-            }
-
-            free(path);
-            token = strtok(NULL, ":");
-        }
-
-        if (found)
-        {
-            if (execve(path, arrof_str, envp) == -1)
-            {
-                perror("execve failed");
-                exit(EXIT_FAILURE);
-            }
-        }
-        else
-        {
-            printf("%s: command not found\n", command);
-            exit(EXIT_FAILURE);
         }
     }
     else if (child_process == -1)
