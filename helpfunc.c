da7356afd298ede55ecc4f55505b71f518cf9fb4
PrincewillDev <princewillfidelis1@gmail.com>
2023-10-18 17:58:37 -0700

    Solved some memory leaks problem

--- helpfunc.c
+++ helpfunc.c
@@ -34,48 +34,50 @@ int numWords(char *str, char *d)
 	return (num);
 }
 
+
 /**
  * strword - function to change string to words
- * @str: line that I want to divid it into words
- * @d: delimter
+ * @str: line that I want to divide into words
+ * @d: delimiter
  * Return: Array of strings
  */
+
 char **strword(char *str, char *d)
 {
-	int numTokens = 0, i = 0, j = 0;
-	int numChar, tmp_i;
-	char **Tokens;
+    int numTokens = 0, i = 0, j = 0;
+    int numChar, tmp_i;
+    char **Tokens;
 
-	if (str == NULL)
-		return (NULL);
+    if (str == NULL)
+        return (NULL);
 
-	numTokens = numWords(str, d);
-	if (numTokens == 0)
-		return (NULL);
+    numTokens = numWords(str, d);
+    if (numTokens == 0)
+        return (NULL);
 
-	Tokens = malloc(sizeof(char *) * (numTokens + 1));
-	if (Tokens == NULL)
-		return (NULL);
+    Tokens = malloc(sizeof(char *) * (numTokens + 1));
+    if (Tokens == NULL)
+        return (NULL);
 
-	for (i = 0, j = 0; j < numTokens; j++)
-	{
-		numChar = 0;
-		while (check_del(str[i], d))
-			i++;
-		while (!check_del(str[i + numChar], d) && str[i + numChar])
-			numChar++;
-		Tokens[j] = malloc(sizeof(char) * (numChar + 1));
-		if (Tokens[j] == NULL)
-		{
-			for (tmp_i = 0; tmp_i < j; tmp_i++)
-				free(Tokens[tmp_i]);
-			free(Tokens);
-			return (NULL);
-		}
-		for (tmp_i = 0; tmp_i < numChar; tmp_i++)
-			Tokens[j][tmp_i] = str[i++];
-		Tokens[j][tmp_i] = '\0';
-	}
-	Tokens[j] = NULL;
-	return (Tokens);
+    for (i = 0, j = 0; j < numTokens; j++)
+    {
+        numChar = 0;
+        while (check_del(str[i], d))
+            i++;
+        while (!check_del(str[i + numChar], d) && str[i + numChar])
+            numChar++;
+        Tokens[j] = malloc(sizeof(char) * (numChar + 1));
+        if (Tokens[j] == NULL)
+        {
+            perror("Memory allocation failed");
+            return (NULL);
+        }
+        for (tmp_i = 0; tmp_i < numChar; tmp_i++)
+            Tokens[j][tmp_i] = str[i++];
+        Tokens[j][tmp_i] = '\0';
+    }
+    Tokens[j] = NULL;
+
+    return (Tokens);
 }
+
--- shell.c
+++ shell.c
@@ -10,37 +10,47 @@
 extern char **environ;
 int main(int argc, char **argv)
 {
-	char *linebuffer;
+	char *linebuffer = NULL;
 	size_t n = 0;
 	ssize_t num_chars;
 	char *delim = " \n";
-	int n_tokens = 0;
+	/*int n_tokens = 0;*/
 	(void)argc;
 
 	while (true)
-	{
-		/* Printing prompt and get input from user*/
-		if (isatty(STDIN_FILENO))
-			print_prompt();
-		num_chars = getline(&linebuffer, &n, stdin);
-		/* Handling EOF or Ctrl D */
-		if (num_chars == -1)
-			break;
+        {
+                /* Printing prompt and get input from user*/
+                if (isatty(STDIN_FILENO))
+                        print_prompt();
+                num_chars = getline(&linebuffer, &n, stdin);
+                /* Handling EOF or Ctrl D */
+                if (num_chars == -1)
+                        break;
 
-		n_tokens = numWords(linebuffer, delim);
-		/*Allocate memory for argv: This memory location for pointer to pointer */
-		argv = malloc(sizeof(char *) * (n_tokens + 1));
-		if (argv == NULL)
-		{
-			perror("hsh: memory allocation failed");
-			return (2);
-		}
-		/*Note: I think we can use linked list here*/
-		argv = strword(linebuffer, delim);
-		executing(argv, environ);
-		free(argv);
-	}
-	/*free memory*/
-	free(linebuffer);
+                /*n_tokens = numWords(linebuffer, delim);*/
+                /*Allocate memory for argv: This memory location for pointer to pointer */
+                /*argv = malloc(sizeof(char *) * (n_tokens + 1));*/
+
+                argv = NULL;
+
+                if (linebuffer != NULL)
+                {
+                        /*n_tokens = numWords(linebuffer, delim);*/
+                        argv = strword(linebuffer, delim);
+                }
+
+                /*argv = strword(linebuffer, delim);*/
+                if (argv == NULL)
+                {
+                        perror("hsh: memory allocation failed");
+                        free(linebuffer);
+                        return (2);
+                }
+                /*Note: I think we can use linked list here*/
+                executing(argv, environ);
+                free(argv);
+        }
+        /*free memory*/
+        free(linebuffer);
 	return (0);
 }
